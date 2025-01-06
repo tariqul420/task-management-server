e66e8f64e628ba6e116e6fd051993fc73c5ae846
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-06 16:20:55 +0600

    first time setup

---
+++ index.js
@@ -0,0 +1,133 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://service-orbit.web.app',
+        'https://service-orbit.firebaseapp.com'
+    ],
+    credentials: true,
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@tariqul-islam.mchvj.mongodb.net/?retryWrites=true&w=majority&appName=TARIQUL-ISLAM`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
+        console.log("☘️  You successfully connected to MongoDB!");
+
+        // Database Collection Name
+        const db = client.db('DatabaseName')
+        const usersCollection = db.collection('Users')
+
+
+        // Verify Jwt Token
+        const verifyToken = async (req, res, next) => {
+            const token = req.cookies.TokenName
+            if (!token) return res.status(401).send({ error: 'unauthorized access' })
+
+            // Verify Token
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                if (error) return res.status(401).send({ error: 'unauthorized access' })
+
+                req.user = decoded
+                next()
+            })
+        }
+
+        // verify Admin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req?.user?.email
+            const query = { email };
+            const user = await usersCollection.findOne(query)
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                res.status(403).send({ message: 'forbidden access' });
+            }
+            next()
+        }
+
+        // Create Jwt Token
+        app.post('/jwt', async (req, res) => {
+            try {
+                const userInfo = req.body
+                const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+                res.cookie('TokenName', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                }).send({ success: true })
+            } catch (err) {
+                console.error('JWT:', err.message)
+                res.status(500).send({ error: 'Failed to create jwt token' })
+            }
+        })
+
+        //logout when not access jwt token
+        app.post('/logout', async (req, res) => {
+            try {
+                res.clearCookie('ServiceOrbit_Token', {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+                }).send({ success: true })
+            } catch (error) {
+                console.error('Logout:', err.message)
+                res.status(500).send({ error: 'Failed to logout when not access jwt token' })
+            }
+        })
+
+        app.post('/users', async (req, res) => {
+            try {
+                const user = req.body
+                const result = await usersCollection.insertOne(user)
+                res.send(result)
+            } catch (error) {
+                console.error('Post User:', error.message)
+                res.status(500).send({ error: 'Failed to post user' })
+            }
+        })
+
+        app.get('/users', async (req, res) => {
+            try {
+                const result = await usersCollection.find().toArray()
+                res.send(result)
+            } catch (error) {
+                console.error('Get User:', error.message)
+                res.status(500).send({ error: 'Failed to get user' })
+            }
+        })
+
+    } catch (err) {
+        console.error('Mongodb', err.message)
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Programmer. How Are You? This Server For No-Name Website ❤️')
+})
+
+app.listen(port, () => {
+    console.log(`☘️  You successfully connected to Server: ${port}`);
+})
---
+++ readme.md
@@ -0,0 +1,21 @@
+## Installation
+
+```bash
+git clone https://github.com/programming-hero-web-course2/b10a11-server-side-tariqul420.git
+```
+
+```bash
+cd b10a11-server-side-tariqul420
+```
+
+```bash
+npm install
+```
+
+```bash
+code .
+```
+
+```bash
+node index.js
+```
